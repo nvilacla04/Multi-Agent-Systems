31598af3ca5d867fe6968c1256b3cfd0af1995e7
Nicolau Vilaclara <nicolaurve@gmail.com>
2025-10-30 11:08:03 +0100

    Assignment 1

---
+++ assignment-1/LOTR.pl
@@ -0,0 +1,13 @@
+fantasy_novel('The Lord of The Rings').
+film_trilogy('The Lord of The Rings'). 
+written_by(tolkein). 
+fantasy_writer(tolkein).
+
+%ture if remains uncorrupted false if corrupted
+corruptionCheck(Being, one_ring) :- pure_heart(Being).
+safeBearer(Being, one_ring) :- corruptionCheck(Being, one_ring),\+ uses_power(Being, one_ring).
+canDestroy(Being, one_ring) :- safeBearer(Being, one_ring).
+
+pure_heart(frodo).
+pure_heart(sam).
+
