bee0bbc48fd04404fc29b37f0fd603396bcc8258
Nicolau Vilaclara <nicolaurve@gmail.com>
2025-10-30 11:45:18 +0100

    Task 1 and 2

--- assignment-1/LOTR.pl
+++ assignment-1/LOTR.pl
@@ -4,10 +4,24 @@ written_by(tolkein).
 fantasy_writer(tolkein).
 
 %ture if remains uncorrupted false if corrupted
-corruptionCheck(Being, one_ring) :- pure_heart(Being).
+corruptionCheck(Being, one_ring) :- \+ pure_heart(Being).
 safeBearer(Being, one_ring) :- corruptionCheck(Being, one_ring),\+ uses_power(Being, one_ring).
 canDestroy(Being, one_ring) :- safeBearer(Being, one_ring).
 
+%hobbits
 pure_heart(frodo).
 pure_heart(sam).
 
+uses_power(gollum, one_ring).
+
+/*
+?- corruptionCheck(sam, one_ring).
+true.
+
+
+
+
+
+
+
+*/
